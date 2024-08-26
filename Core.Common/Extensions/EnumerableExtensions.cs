0a5fafd6ad52235671aaeb73beb0920117fc04aa
caio_dom <caio_henrrique@hotmail.com>
2024-08-26 14:28:21 -0300

    Enum extensions

---
+++ Core.Common/Extensions/EnumerableExtensions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Common.Extensions
+{
+    public static class EnumerableExtensions
+    {
+        public static void ForEach<T> (this IEnumerable<T> items, Action<T> itemAction)
+        {
+            foreach(var item in items)
+                itemAction(item);
+        }
+
+        public static string GetEnumDescripton<TEnum>(this TEnum item)
+        {
+            return item.GetType()
+                       .GetField(item.ToString())
+                       .GetCustomAttributes(typeof(DescriptionAttribute),false)
+                       .Cast<DescriptionAttribute>()
+                       .FirstOrDefault()?.Description ?? string.Empty;
+        }
+    }
+}
